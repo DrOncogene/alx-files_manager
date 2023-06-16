291c55000384c9b2c59737638c857628820a4770
Nasir Abdulrasheed <mypythtesting@gmail.com>
2023-06-16 19:52:38 +0100

    implemented a new route POST /users to add new users

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import { getStats, getStatus } from '../controllers/AppController';
+import postNew from '../controllers/UsersController'
 
 const router = express.Router();
 
@@ -7,7 +8,8 @@ router.use((req, res, next) => {
   next();
 });
 
-router.use('/status', getStatus);
-router.use('/stats', getStats);
+router.get('/status', getStatus);
+router.get('/stats', getStats);
+router.post('/users', postNew);
 
 export default router;
